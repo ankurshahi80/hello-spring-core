dd3efd23940cb16204d7a9b81af41c47c55a564b
Ankur Shahi <shahiankur80@gmail.com>
2022-04-08 21:31:34 -0400

    created Pokemon model with lombok annotation

---
+++ src/main/java/com/revature/models/Pokemon.java
@@ -0,0 +1,24 @@
+package com.revature.models;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+//@Data combines @Getter, @Setter and @ToString
+@RequiredArgsConstructor
+@EqualsAndHashCode
+public class Pokemon {
+    private int id;
+    private String pokemonName;
+    private String pokeType;
+    private int attackPower;
+
+    public Pokemon(String pokemonName, String pokeType, int attackPower) {
+        this.pokemonName = pokemonName;
+        this.pokeType = pokeType;
+        this.attackPower = attackPower;
+    }
+}
